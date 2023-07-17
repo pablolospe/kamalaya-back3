210b0f4c864912b4af586c19acba76c5f8880d47
pablolospe <yosoypxl@gmail.com>
2023-07-16 21:52:03 -0300

    CRUD usuarios

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,21 +5,56 @@ const router = Router();
 
 router.get('/usuarios', async (req, res) => {
   try {
-    const usuarios = await 
-    Usuario.findAll();
+    const usuarios = await Usuario.findAll();
     res.status(200).json(usuarios);
   } catch (error) {
     res.status(404).json(error);
   }
 });
 
+router.get('/usuarios/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const usuario = await Usuario.findByPk(id);
+    res.status(200).json(usuario);
+  } catch (error) {
+    res.status(404).json(error);
+  }
+});
+
 router.post('/usuarios', async (req, res) => {
-    const data = req.body;
-    try {
+  const data = req.body;
+  try {
     console.log(data);
     const nuevoUsuario = await Usuario.create(data);
-    // console.log(nuevoUsuario);
-    res.status(200).json(data);
+    res.status(200).json(nuevoUsuario);
+  } catch (error) {
+    res.status(404).json(error);
+  }
+});
+
+router.delete('/usuarios/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const usuarioABorrar = await Usuario.findByPk(id);
+    if (!usuarioABorrar) res.status(200).send('Usuario no encontrado');
+    else {
+      await Usuario.destroy({ where: { usuario_id: id } });
+      res.status(200).json(usuarioABorrar);
+    }
+  } catch (error) {
+    res.status(404).json(error);
+  }
+});
+
+router.put('/usuarios/:id', async (req, res) => {
+    try {
+    const { id } = req.params;
+    const data = req.body;
+    const usuario = await Usuario.findByPk(id);
+    
+    await usuario.update(data)
+    res.status(200).json(usuario);
   } catch (error) {
     res.status(404).json(error);
   }
