319b07a033508f96ba75a5f3e4aa1ad65dbd9987
pablolospe <yosoypxl@gmail.com>
2023-07-16 21:18:29 -0300

    post?

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,23 @@ const { Usuario } = require('../db/db');
 
 const router = Router();
 
-router.get('/', async (req, res) => {
+router.get('/usuarios', async (req, res) => {
   try {
     const usuarios = await 
     Usuario.findAll();
     res.status(200).json(usuarios);
+  } catch (error) {
+    res.status(404).json(error);
+  }
+});
+
+router.post('/usuarios', async (req, res) => {
+    const data = req.body;
+    try {
+    console.log(data);
+    const nuevoUsuario = await Usuario.create(data);
+    // console.log(nuevoUsuario);
+    res.status(200).json(data);
   } catch (error) {
     res.status(404).json(error);
   }
