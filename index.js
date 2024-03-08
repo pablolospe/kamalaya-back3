95a3485fa356b68e74b23d7ff1e51cce4026d448
Pablo Xavier Lospennato <yosoypxl@gmail.com>
2024-03-08 03:48:59 +0000

    force true!

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./src/server')
 const { conn } = require('./src/db/db.js');
 const { Voluntario, User, Paciente } = require("./src/db/db");
 
-conn.sync({force:false}).then(() => {
+conn.sync({force:true}).then(() => {
     app.listen(8000, () => {
       console.log(`Server running...`);
 
