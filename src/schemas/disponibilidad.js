4bcd65cf36ea60e4c639b74fd37ce6464735b939
pablolospe <yosoypxl@gmail.com>
2024-05-17 12:03:11 -0300

    disponibilidades comentadas

--- src/schemas/disponibilidad.js
+++ src/schemas/disponibilidad.js
@@ -2,6 +2,10 @@ const z = require('zod');
 
 const disponibilidadSchema = z.object({
   diaSemana: z.enum(['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo']),
+  // tipo: z.enum(['telefonica', 'presencial', 'admisión']),
+  // acompTelefonico: z.boolean(),
+  // acompPresencial: z.boolean(),
+  // admisiones: z.boolean(),
   horaInicio: z.string(),
   horaFin: z.string(),
   voluntario_id: z.any(),
