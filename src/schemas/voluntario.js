7ff3d5a021773be32f95b71b4e4f3c9de2ab00da
pablolospe <yosoypxl@gmail.com>
2024-05-29 11:26:12 -0300

    nombre y direccion volunt toLowercase

--- src/schemas/voluntario.js
+++ src/schemas/voluntario.js
@@ -1,25 +1,31 @@
 const z = require('zod');
 
+const toLowerCase = (str) => str.toLowerCase();
+
 const voluntarioSchema = z.object({
   nombre: z.string({
-    invalid_type_error: 'Nombre debe ser un string',
-  }),
-  apellido: z.string(),
+    invalid_type_error: 'El nombre debe ser un string',
+    required_error: 'El nombre es requerido'
+  }).toLowerCase(),
+  apellido: z.string({
+    invalid_type_error: 'El apellido debe ser un string',
+    required_error: 'El apellido es requerido'
+  }).toLowerCase(),
   dni: z.string(),
-  email: z.string({ required_error: 'El email es requerido' }),
+  email: z.string({ required_error: 'El email es requerido' }).toLowerCase(),
   telefono: z.string(),
   telefono2: z.string(),
-  calle: z.string(),
+  calle: z.string().toLowerCase(),
   numero: z.string(),
-  departamento: z.string(),
-  localidad: z.string(),
-  provincia: z.string(),
+  departamento: z.string().toLowerCase(),
+  localidad: z.string().toLowerCase(),
+  provincia: z.string().toLowerCase(),
   lat: z.string(),
   lng: z.string(),
-  pais: z.string(),
+  pais: z.string().toLowerCase(),
   codigoPostal: z.string(),
   telefonoEmergencia: z.string(),
-  nombreContactoEmergencia: z.string(),
+  nombreContactoEmergencia: z.string().toLowerCase(),
   genero: z.enum(['M', 'F', 'otro']),
   profesion_oficio_ocupacion: z.string(),
   hobbies_habilidades: z.string(),
@@ -36,5 +42,6 @@ function validarVoluntario(object) {
 }
 
 module.exports = { validarVoluntario };
+
 
 
